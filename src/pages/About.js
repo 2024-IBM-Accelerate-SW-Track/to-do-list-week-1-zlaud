c19d95ffed2752a9048ff685f6827a82048c7238
zlaud <laudkimberly@gmail.com>
2024-06-06 23:18:04 -0700

    initial commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,34 @@
-import React, { Component } from 'react';
-  
+import React, { Component } from "react";
+import "./About.css";
+import profile from "../assets/profile.jpg";
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+          <div className="split left">
+            <div className="centered">
+              <img
+                className="profile_image"
+                src={profile}
+                alt="Profile Pic"
+              ></img>
+            </div>
+          </div>
+          <div className="split right">
+            <div className="centered">
+              <div className="name_title">Zophia Laud</div>
+              <div className="brief_description">
+                My name is Zophia, and I'm an incoming third-year computer
+                science. I have an interest in full-stack development and
+                machine learning. In my free time, I like to read books, play
+                video games, and going for drives.
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
